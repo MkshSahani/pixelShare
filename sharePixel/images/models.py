99a2ad343ef5e11206fe05e4d947e9cd4d154d8f
MkshSahani <msco18333@gmail.com>
2021-03-12 17:30:38 +0530

    updated

--- sharePixel/images/models.py
+++ sharePixel/images/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings 
+from django.utils.text import slugify
 
-# Create your models here.
+class Images(models.Model): 
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="user_images", on_delete=models.CASCADE) 
+    title = models.CharField(max_length=200) 
+    url = models.URLField()
+    img = models.ImageField(upload_to='user_images') 
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+    user_liked = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_like', blank=True)
+
+
+    def save(self, *args, **kwargs): 
+        if not self.slug: 
+            self.slug = slugify(self.title)
+            super(Images, self).save(*args, **kwargs)
+
+    def __str__(self): 
+        return self.title 
+
+    
